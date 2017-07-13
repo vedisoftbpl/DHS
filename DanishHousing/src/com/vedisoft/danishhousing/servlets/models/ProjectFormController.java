342306e19c0d8878d005730aa3002a58e3faf18e
Aman Kumar <kumaraman334@gmail.com>
2017-07-14 02:27:19 +0530

    ProjectFormController Updated(Show and Edit operation added,Enum added)

--- DanishHousing/src/com/vedisoft/danishhousing/servlets/models/ProjectFormController.java
+++ DanishHousing/src/com/vedisoft/danishhousing/servlets/models/ProjectFormController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.vedisoft.danishhousing.daos.ProjectsDao;
+import com.vedisoft.danishhousing.pojos.ProjectTypeEnum;
 import com.vedisoft.danishhousing.pojos.Projects;
 
 /**
@@ -18,94 +19,99 @@ import com.vedisoft.danishhousing.pojos.Projects;
 @WebServlet("/admin/pages/ProjectFormController")
 public class ProjectFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProjectFormController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ProjectFormController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		int id =0;
-		if(request.getParameter("showId") != null && request.getParameter("showId").trim().length() > 0) {
-			 id = Integer.parseInt(request.getParameter("showId"));
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int id = 0;
+		if (request.getParameter("showId") != null && request.getParameter("showId").trim().length() > 0) {
+			id = Integer.parseInt(request.getParameter("showId"));
 		}
-		
-		
-		
-		
-		
-		
+
 		String projectName = new String();
-		if(request.getParameter("ProjectName") != null && request.getParameter("ProjectName").trim().length() > 0) {
+		if (request.getParameter("ProjectName") != null && request.getParameter("ProjectName").trim().length() > 0) {
 			projectName = request.getParameter("ProjectName");
 		}
-		
+
 		String operation = new String();
-		if(request.getParameter("operation") != null && request.getParameter("operation").trim().length() > 0) {
+		if (request.getParameter("operation") != null && request.getParameter("operation").trim().length() > 0) {
 			operation = request.getParameter("operation");
 		}
-		
-		char projType = ' ';
-		if(request.getParameter("ProjectType") != null && request.getParameter("ProjectType").trim().length() > 0)
-			projType =  request.getParameter("ProjectType").charAt(0);
-		
+
+		String projType = new String();
+		if (request.getParameter("ProjectType") != null && request.getParameter("ProjectType").trim().length() > 0)
+			projType = request.getParameter("ProjectType");
+
 		System.out.println(projectName + " " + projType);
-		
+
 		String page = "/pages/admin/ProjectForm.jsp";
-		String page1= "/pages/admin/ProjectViewForm.jsp";
-		if(operation.equals("create")){
+		String page1 = "/pages/admin/ProjectViewForm.jsp";
+
+		request.setAttribute("enumList", ProjectTypeEnum.values());
+
+		if (operation.equals("create")) {
 			ProjectsDao dao = new ProjectsDao();
 			Projects p = new Projects(projectName, projType);
 			System.out.println(p);
 			int a = 0;
 			a = dao.create(p);
-			if(a > 0)
-				response.sendRedirect("/DanishHousing" + page + "?msg=1");
-			else
-				response.sendRedirect("/DanishHousing" + page + "?msg=2");
-			//RequestDispatcher rd = request.getRequestDispatcher(page);
-			//rd.forward(request, response);
-		}
-		else if(operation.equals("show")){
+			if (a > 0) {
+				RequestDispatcher rd = request.getRequestDispatcher(page);
+				request.setAttribute("msg", 1);
+				rd.forward(request, response);
+
+			} else {
+				RequestDispatcher rd = request.getRequestDispatcher(page);
+				request.setAttribute("msg", 2);
+				rd.forward(request, response);
+			}
+		} else if (operation.equals("show")) {
 			ProjectsDao dao = new ProjectsDao();
 			Projects p = new Projects();
 			p = dao.find(id);
-			request.setAttribute("project",p);
+			request.setAttribute("project", p);
 			RequestDispatcher rd = request.getRequestDispatcher(page1);
 			rd.forward(request, response);
-		}
-		else if(operation.equals("edit")){
+		} else if (operation.equals("edit")) {
 			ProjectsDao dao = new ProjectsDao();
-			Projects p = new Projects(id,projectName, projType);
+			Projects p = new Projects(id, projectName, projType);
 			System.out.println(p);
 			Boolean b = dao.edit(p);
-			if(b)
-				response.sendRedirect("/DanishHousing" + page1 + "?msg=1");
-			else
-				response.sendRedirect("/DanishHousing" + page1 + "?msg=2");
-				
-		}
-		else {
+			if (b) {
+				RequestDispatcher rd = request.getRequestDispatcher(page);
+				request.setAttribute("msg", 1);
+				rd.forward(request, response);
+			} else {
+				RequestDispatcher rd = request.getRequestDispatcher(page);
+				request.setAttribute("msg", 2);
+				rd.forward(request, response);
+			}
+		} else {
 			RequestDispatcher rd = request.getRequestDispatcher(page);
 			rd.forward(request, response);
 		}
-		
-		
-		
+
 	}
 
 }
