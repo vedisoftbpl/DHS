3886866abdd9e9c7b47dc684bdc92731e919611c
vedisoft <vedisoft@vedisoft>
2017-07-20 14:11:06 +0530

    In pojos, made ReceiptsRecord.java file

---
+++ DanishHousing/src/com/vedisoft/danishhousing/pojos/ReceiptRecords.java
@@ -0,0 +1,46 @@
+package com.vedisoft.danishhousing.pojos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ReceiptRecords implements Serializable{
+
+	private int recordId; 		//1
+	private int sNo;			//2	
+	private char payType;		//3
+	private int sIno;			//4
+	private Date recdte;		//5
+	private int receno;		//6
+	private String prefix;		//7
+	private String membnme;		//8
+	private int membno;		//9
+	private String fHNme;		//10
+	private String mad1;		//11
+	private String mad2;		//12
+	private String mad3;		//13
+	private double cashamt;	//14
+	private double chqamt;		//15
+	private double balchq;		//16
+	private int cDd;			//17
+	private Date cDdte;			//18
+	private double fullpay	;	//19
+	private double inst1;		//20
+	private double inst2;		//21
+	private double inst3;		//22
+	private int chalno;		//23
+	private Date chaldte;		//24
+	private String plSize;		//25
+	private String plno;		//26
+	private int projcd;		//27
+	private String chqdhr;		//28
+	private String flag;		//29
+	private char trcode;		//30
+	private String remarks;		//31
+	private char rC;			//32
+	private String pD;			//33
+	private String accode;		//34
+	private String branch;		//35
+	private char dC;			//36
+	private Date wcLrDc;		//37
+	
+}
