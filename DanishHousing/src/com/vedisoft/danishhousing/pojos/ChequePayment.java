5d9742174dcaebb293e658c46a6865451effe18f
Abhishek <abhishek001.cs@gmail.com>
2017-08-20 17:13:44 +0530

    ChequePaymentPojo

---
+++ DanishHousing/src/com/vedisoft/danishhousing/pojos/ChequePayment.java
@@ -0,0 +1,81 @@
+package com.vedisoft.danishhousing.pojos;
+
+import java.util.Date;
+
+public class ChequePayment {
+
+	private int docno;
+	private Date docDate;
+	private String bankCode;
+	private String paymentMode;
+	private String transactionNo;
+	private Double amount;
+	private Date cheqClDate;
+	public ChequePayment() {
+		super();
+	}
+	public ChequePayment(int docno, Date docDate, String bankCode, String paymentMode, String transactionNo,
+			Double amount, Date cheqClDate) {
+		super();
+		this.docno = docno;
+		this.docDate = docDate;
+		this.bankCode = bankCode;
+		this.paymentMode = paymentMode;
+		this.transactionNo = transactionNo;
+		this.amount = amount;
+		this.cheqClDate = cheqClDate;
+	}
+	public int getDocno() {
+		return docno;
+	}
+	public void setDocno(int docno) {
+		this.docno = docno;
+	}
+	public Date getDocDate() {
+		return docDate;
+	}
+	public void setDocDate(Date docDate) {
+		this.docDate = docDate;
+	}
+	public String getBankCode() {
+		return bankCode;
+	}
+	public void setBankCode(String bankCode) {
+		this.bankCode = bankCode;
+	}
+	public String getPaymentMode() {
+		return paymentMode;
+	}
+	public void setPaymentMode(String paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+	public String getTransactionNo() {
+		return transactionNo;
+	}
+	public void setTransactionNo(String transactionNo) {
+		this.transactionNo = transactionNo;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public Date getCheqClDate() {
+		return cheqClDate;
+	}
+	public void setCheqClDate(Date cheqClDate) {
+		this.cheqClDate = cheqClDate;
+	}
+	@Override
+	public String toString() {
+		return "ChequePayment [docno=" + docno + ", docDate=" + docDate + ", bankCode=" + bankCode + ", paymentMode="
+				+ paymentMode + ", transactionNo=" + transactionNo + ", amount=" + amount + ", cheqClDate=" + cheqClDate
+				+ "]";
+	}
+	
+	
+	
+	
+	
+}
